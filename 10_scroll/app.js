18a73e6b899c91b5154dc73b856c67f2ba1c30c7
Esben <esbeneickhardt@gmail.com>
2024-08-21 14:13:08 +0200

    Added back-to-top button and a static navbar when scrolling

--- 10_scroll/app.js
+++ 10_scroll/app.js
@@ -1,13 +1,57 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+// Set footer date dynamically
+const date = document.querySelector(".date")
+const currentYear = new Date().getFullYear();
 
-// ********** set date ************
+window.addEventListener('DOMContentLoaded', function() {
+    date.textContent = currentYear;
+})
 
-// ********** close links ************
+// Setting up navbar links toggle
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const numberOfLinks = links.querySelectorAll("a").length;
 
-// ********** fixed navbar ************
+// Static Height
+// navToggle.addEventListener('click', function() {
+//     linksContainer.classList.toggle("show-links")
+// })
 
-// ********** smooth scroll ************
-// select links
+// Dynamic Height
+navToggle.addEventListener('click', function() {;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    linksContainer.style.height = `${linksHeight}px`;
+
+    if(containerHeight == 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = `0px`;
+    }
+})
+
+// Navbar with always stay even when scrolling
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link")
+
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.scrollY;
+    const navbarHeight = navbar.getBoundingClientRect().height;
+
+    // When to replace navbar with a static navbar
+    if (scrollHeight > navbarHeight) {
+        navbar.classList.add('fixed-nav')
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+    // When to display "back to top link"
+    if (scrollHeight > navbarHeight) {
+        topLink.classList.add("show-link");
+      } else {
+        topLink.classList.remove("show-link");
+      }
+})
+
+// Aligning headers with scroll
+
