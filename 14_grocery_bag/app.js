7df52dae48e2e09556f46a856d6ca172c07f2028
Esben <esbeneickhardt@gmail.com>
2024-09-15 21:03:10 +0200

    Local storage added

--- 14_grocery_bag/app.js
+++ 14_grocery_bag/app.js
@@ -28,11 +28,8 @@ form.addEventListener('submit', addItem);
 // Clearing list
 clearBtn.addEventListener('click', clearItems);
 
-// Edit item
-
-// Delete item
-// const deleteBtn = document.querySelector('.delete-btn');
-// console.log(deleteBtn)
+// Loading local storage
+window.addEventListener('DOMContentLoaded', setupItems);
 
 ///////////////////////
 ////// FUNCTIONS //////
@@ -49,6 +46,102 @@ function addItem(e) {
   
   // Adding item to list
   if(value && editFlag === false){
+    // Creates list item
+    createListItem(id, value);
+
+    // Show container
+    container.classList.add("show-container");
+
+    // Displaying alert
+    displayAlert("Adding Item", "success")
+
+    // Add to local storage
+    addToLocalStorage(id, value);
+
+    // Set text form back to default
+    setBackToDefault();
+  } 
+  // Editing item
+  else if (value && editFlag) {
+    // Updating element
+    editElement.innerHTML = value;
+
+    // Updating local storage
+    editLocalStorage(editId, value);
+
+    // Set text form back to default
+    setBackToDefault();
+  } else {
+    displayAlert("Missing Item", "danger")
+  }
+};
+
+// Delete function
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove("show-container");
+  }
+  setBackToDefault();
+
+  // Removes from local storage
+  removeFromLocalStorage(id);
+};
+
+// Edit function
+function editItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+
+  // Set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+
+  // Set form item
+  grocery.value = editElement.innerHTML
+  editFlag = true;
+  editId = element.dataset.id;
+  submit.textContent = "edit";
+};
+
+// Clearing all items
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+  container.classList.remove("show-container");
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  setBackToDefault();
+
+  // Deletes items in local storage
+  localStorage.removeItem("list"); 
+};
+
+// Creating alert
+function displayAlert(text,action) {
+  // add alert
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+};
+
+// Sets text form back to default
+function setBackToDefault() {
+  grocery.value = '';
+  editFlag = false;
+  editId = '';
+  submit.textContent = 'submit';
+};
+
+// Creates an grocery item
+function createListItem(id, value) {
     // Creating element to store item
     const element = document.createElement("article");
     element.classList.add('grocery-item');
@@ -77,102 +170,61 @@ function addItem(e) {
 
     // Adding element to grocery-list
     list.appendChild(element);
+}
 
-    // Show container
-    container.classList.add("show-container");
+///////////////////////////
+////// Local Storage //////
+///////////////////////////
 
-    // Displaying alert
-    displayAlert("Adding Item", "success")
+// Setup Items
+function setupItems() {
+  let items = getLocalStorage();
 
-    // Add to local storage
-    // addToLocalStorage(id, value);
-
-    // Set text form back to default
-    setBackToDefault();
-  } 
-  // Editing item
-  else if (value && editFlag) {
-    // Updating element
-    editElement.innerHTML = value;
-
-    setBackToDefault();
-  } else {
-    displayAlert("Missing Item", "danger")
-  }
-};
-
-// Delete function
-function deleteItem(e) {
-  const element = e.currentTarget.parentElement.parentElement;
-  const id = element.dataset.id;
-  list.removeChild(element);
-  if (list.children.length === 0) {
-    container.classList.remove("show-container");
-  }
-  setBackToDefault();
-  //removeFromLocalStorage();
-};
-
-// Edit function
-function editItem(e) {
-  const element = e.currentTarget.parentElement.parentElement;
-
-  // Set edit item
-  editElement = e.currentTarget.parentElement.previousElementSibling;
-
-  // Set form item
-  grocery.value = editElement.innerHTML
-  editFlag = true;
-  editId = element.dataset.id;
-  submit.textContent = "edit";
-  
-};
-
-// Clearing all items
-function clearItems() {
-  const items = document.querySelectorAll(".grocery-item");
-  container.classList.remove("show-container");
   if (items.length > 0) {
-    items.forEach(function (item) {
-      list.removeChild(item);
+    items.forEach(function(item) {
+      createListItem(item.id, item.value)
     });
+    container.classList.add("show-container");
   }
-  setBackToDefault();
-  //localStorage.removeItem("list"); 
-};
+}
 
-// Creating alert
-function displayAlert(text,action) {
-  // add alert
-  alert.textContent = text;
-  alert.classList.add(`alert-${action}`);
+// Gets storage
+function getLocalStorage() {
+  const storedList = localStorage.getItem("list");
+  if (storedList) {
+    return JSON.parse(storedList);
+  } else {
+    return [];
+  }
+}
 
-  // remove alert
-  setTimeout(function () {
-    alert.textContent = "";
-    alert.classList.remove(`alert-${action}`);
-  }, 1000);
-};
-
-// Sets text form back to default
-function setBackToDefault() {
-  grocery.value = '';
-  editFlag = false;
-  editId = '';
-  submit.textContent = 'submit';
-};
-
-// Adds items to local storage
+// Adds item to local storage
 function addToLocalStorage(id, value) {
-  console.log("added to local storage")
-};
+  const groceryItem = { id, value };
+  let items = getLocalStorage();
+  items.push(groceryItem);
+  localStorage.setItem("list", JSON.stringify(items));
+}
 
-// Sets text form back to default
+// Removes item from local storage
 function removeFromLocalStorage(id) {
-  console.log("removed from local storage")
-};
+  let items = getLocalStorage();
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
 
-// Sets text form back to default
+// Edits item in local storage
 function editLocalStorage(id, value) {
-  console.log("edited local storage")
-};
+  let items = getLocalStorage();
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
